4931ed6a09c6e410668fb13657fe9735578b1bdb
Ignacio Jonas <ignaciojonas@gmail.com>
2017-05-25 15:37:52 -0300

    Async - Ejemplo

---
+++ Async/js/createButtons.js
@@ -0,0 +1,18 @@
+document.getElementById('create').addEventListener('click',createButtons);
+function createButtons() {
+  let quantity = document.getElementById('quantity').value;
+  for (let i = 0; i < quantity; i++) {
+    let randTime = Math.random() * 5000;
+    setTimeout((name = i) => { createButton(name) }, randTime);
+
+  }
+}
+
+function createButton(name){
+  let div = document.getElementById('buttons');
+  let button = document. createElement("button");
+  button.name = name;
+  button.innerHTML = "Button " + name;
+  button.addEventListener('click',() => { alert('This is the button: ' + name) })
+  div.appendChild(button);
+}
